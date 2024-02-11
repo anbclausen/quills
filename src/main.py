b669e17d7aa1cf56315559b96af9c5c3e866c93f
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-02-11 14:15:40 +0100

    feat: added platform class and platform config

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from qiskit import QuantumCircuit
+from quantum_platform import Platform
+import json
+
+with open("src/platforms.json", "r") as file:
+    platforms_data = json.load(file)
+    platform_list = [Platform(data) for data in platforms_data]
+    platforms = {platform.name: platform for platform in platform_list}
+
+print(platforms)
+
 
 circuit = QuantumCircuit.from_qasm_file("benchmarks/toy_example.qasm")
 
