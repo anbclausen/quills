1758ba16e773e5eaa430634802a2a02cc5656472
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-02-12 12:40:15 +0100

    feat: now taking arguments

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
-from test.test_incr import test
+import sys
+
+import test.test_incr as incr
+import test.test_opt as opt
 from solvers import M_SEQUENTIAL_PLANS, MpC_SEQUENTIAL_PLANS
 
-domain, problem = test()
+synthesizer_arg = sys.argv[1]
+match synthesizer_arg:
+    case "opt":
+        domain, problem = opt.test()
+    case "incr":
+        domain, problem = incr.test()
+    case _:
+        raise ValueError(f"Unknown synthesizer '{synthesizer_arg}'")
 
-solver_arg = "MpC"
 
+solver_arg = sys.argv[2]
 match solver_arg:
     case "M":
         solver = M_SEQUENTIAL_PLANS
@@ -14,5 +24,11 @@ match solver_arg:
         raise ValueError(f"Unknown solver '{solver_arg}'")
 
 out, time = solver.solve(domain, problem, 1800)
-print(out)
+
+print("Synthesizer:", synthesizer_arg)
+print("Solver: ", solver_arg)
+print()
+print("###########")
+print(out.strip())
+print("###########")
 print("Took time", f"{time:.03f}s")
