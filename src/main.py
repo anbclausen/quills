33619455a23b090bab00544a086764065c047bb4
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-02-12 09:57:58 +0100

    feat: idea for taking argument for solver

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from test.test_opt import test
-from solvers import M_SEQUENTIAL_PLANS
+from solvers import M_SEQUENTIAL_PLANS, MpC_SEQUENTIAL_PLANS
 
 domain, problem = test()
 
-out, time = M_SEQUENTIAL_PLANS.solve(domain, problem, 1800)
+solver_arg = "MpC"
+
+match solver_arg:
+    case "M":
+        solver = M_SEQUENTIAL_PLANS
+    case "MpC":
+        solver = MpC_SEQUENTIAL_PLANS
+    case _:
+        raise ValueError(f"Unknown solver '{solver_arg}'")
+
+out, time = solver.solve(domain, problem, 1800)
 print(out)
 print("Took time", f"{time:.03f}s")
