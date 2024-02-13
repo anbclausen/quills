0c2776ab0cf3fa22321f8bd027a365d223e5d02e
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-02-13 17:24:52 +0100

    feat: first version of optimal encoding

--- src/synthesizers/optimal_planning.py
+++ src/synthesizers/optimal_planning.py
@@ -1,4 +1,8 @@
-from synthesizers.synthesizer import Synthesizer, gate_input_mapping
+from synthesizers.synthesizer import (
+    Synthesizer,
+    gate_line_dependency_mapping,
+    gate_direct_dependency_mapping,
+)
 from platforms import Platform
 from qiskit import QuantumCircuit
 from src.pddl import PDDLInstance, PDDLAction, PDDLPredicate, object_, not_
@@ -30,47 +34,47 @@ class PlanningSynthesizer(Synthesizer):
         g = [gate(f"g{i}") for i in range(num_gates)]
         d = [depth(f"d{i}") for i in range(maximum_depth)]
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def occupied(p: pqubit):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def mapped(l: lqubit, p: pqubit):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def connected(p1: pqubit, p2: pqubit):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def done(g: gate):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def clock(p: pqubit, d: depth):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def next_depth(d1: depth, d2: depth):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def next_swap_depth(d1: depth, d2: depth):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def is_swapping1(p1: pqubit, p2: pqubit):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def is_swapping2(p1: pqubit, p2: pqubit):
             pass
 
-        @PDDLPredicate
+        @PDDLPredicate()
         def is_swapping(p: pqubit):
             pass
 
-        @PDDLAction
+        @PDDLAction()
         def swap(l1: lqubit, l2: lqubit, p1: pqubit, p2: pqubit, d1: depth, d2: depth):
             preconditions = [
                 mapped(l1, p1),
@@ -95,13 +99,13 @@ class PlanningSynthesizer(Synthesizer):
             ]
             return preconditions, effects
 
-        @PDDLAction
+        @PDDLAction()
         def swap_dummy1(p1: pqubit, p2: pqubit):
             preconditions = [is_swapping1(p1, p2)]
             effects = [not_(is_swapping1(p1, p2)), is_swapping2(p1, p2)]
             return preconditions, effects
 
-        @PDDLAction
+        @PDDLAction()
         def swap_dummy2(p1: pqubit, p2: pqubit):
             preconditions = [is_swapping2(p1, p2)]
             effects = [
@@ -111,15 +115,170 @@ class PlanningSynthesizer(Synthesizer):
             ]
             return preconditions, effects
 
-        @PDDLAction
+        @PDDLAction()
         def nop(p: pqubit, d1: depth, d2: depth):
             preconditions = [next_depth(d1, d2), clock(p, d1), not_(is_swapping(p))]
             effects = [clock(p, d2), not_(clock(p, d1))]
             return preconditions, effects
 
-        gate_mapping = gate_input_mapping(circuit)
+        gate_line_mapping = gate_line_dependency_mapping(circuit)
+        gate_direct_mapping = gate_direct_dependency_mapping(circuit)
 
-        return PDDLInstance()
+        gate_actions = []
+        for gate_id, (gate_type, gate_qubits) in gate_line_mapping.items():
+            no_gate_dependency = gate_direct_mapping[gate_id] == []
+
+            match gate_type:
+                case "cx":
+
+                    @PDDLAction(name=f"apply_cx_g{gate_id}")
+                    def apply_gate(p1: pqubit, p2: pqubit, d1: depth, d2: depth):
+                        preconditions = [
+                            not_(done(g[gate_id])),
+                            next_depth(d1, d2),
+                            connected(p1, p2),
+                            clock(p1, d1),
+                            clock(p2, d1),
+                            not_(is_swapping(p1)),
+                            not_(is_swapping(p2)),
+                        ]
+
+                        one_gate_dependency = len(gate_direct_mapping[gate_id]) == 1
+
+                        if no_gate_dependency:
+                            preconditions.append(not_(occupied(p1)))
+                            preconditions.append(not_(occupied(p2)))
+                        elif one_gate_dependency:
+                            earlier_gate = gate_direct_mapping[gate_id][0]
+                            # find the line that the earlier gate is on
+                            occupied_line_id = (
+                                set(gate_qubits)
+                                .intersection(gate_direct_mapping[earlier_gate])
+                                .pop()
+                            )
+                            occupied_line_index = gate_qubits.index(occupied_line_id)
+
+                            occupied_qubit = p1 if occupied_line_index == 0 else p2
+                            unoccupied_qubit = p2 if occupied_line_index == 0 else p1
+
+                            # preconds for the line that the earlier gate is on
+                            preconditions.append(done(g[earlier_gate]))
+                            preconditions.append(
+                                mapped(l[occupied_line_id], occupied_qubit)
+                            )
+
+                            # preconds for the line that has not had any gates yet
+                            preconditions.append(not_(occupied(unoccupied_qubit)))
+                        else:
+                            preconditions.append(
+                                *[done(g[dep]) for dep in gate_direct_mapping[gate_id]]
+                            )
+                            control_qubit = l[gate_qubits[0]]
+                            target_qubit = l[gate_qubits[1]]
+
+                            preconditions.append(mapped(control_qubit, p1))
+                            preconditions.append(mapped(target_qubit, p2))
+
+                        effects = [
+                            done(g[gate_id]),
+                            not_(clock(p1, d1)),
+                            not_(clock(p2, d1)),
+                            clock(p1, d2),
+                            clock(p2, d2),
+                        ]
+
+                        if no_gate_dependency:
+                            effects.append(occupied(p1))
+                            effects.append(occupied(p2))
+                            effects.append(mapped(l[gate_qubits[0]], p1))
+                            effects.append(mapped(l[gate_qubits[1]], p2))
+                        elif one_gate_dependency:
+                            earlier_gate = gate_direct_mapping[gate_id][0]
+                            # find the line that the earlier gate is on
+                            occupied_line_id = (
+                                set(gate_qubits)
+                                .intersection(gate_direct_mapping[earlier_gate])
+                                .pop()
+                            )
+                            occupied_line_index = gate_qubits.index(occupied_line_id)
+                            unoccupied_line_index = 1 - occupied_line_index
+
+                            unoccupied_qubit = p2 if occupied_line_index == 0 else p1
+
+                            effects.append(occupied(unoccupied_qubit))
+                            effects.append(
+                                mapped(l[unoccupied_line_index], unoccupied_qubit)
+                            )
+
+                        return preconditions, effects
+
+                case _:
+
+                    @PDDLAction(name=f"apply_gate_g{gate_id}")
+                    def apply_gate(p: pqubit, d1: depth, d2: depth):
+                        preconditions = [
+                            not_(done(g[gate_id])),
+                            next_depth(d1, d2),
+                            clock(p, d1),
+                            not_(is_swapping(p)),
+                        ]
+
+                        if no_gate_dependency:
+                            preconditions.append(not_(occupied(p)))
+                        else:
+                            preconditions.append(
+                                *[done(g[dep]) for dep in gate_direct_mapping[gate_id]]
+                            )
+                            preconditions.append(
+                                *[mapped(l[i], p) for i in gate_qubits]
+                            )
+
+                        effects = [
+                            done(g[gate_id]),
+                            clock(p, d2),
+                            not_(clock(p, d1)),
+                        ]
+
+                        if no_gate_dependency:
+                            effects.append(occupied(p))
+                            effects.append(*[mapped(l[i], p) for i in gate_qubits])
+
+                        return preconditions, effects
+
+            gate_actions.append(apply_gate)
+
+        return PDDLInstance(
+            types=[pqubit, lqubit, gate, depth],
+            constants=[*p, *l, *g, *d],
+            predicates=[
+                occupied,
+                mapped,
+                connected,
+                done,
+                clock,
+                next_depth,
+                next_swap_depth,
+                is_swapping1,
+                is_swapping2,
+                is_swapping,
+            ],
+            actions=[
+                swap,
+                swap_dummy1,
+                swap_dummy2,
+                nop,
+                *gate_actions,
+            ],
+            initial_state=[
+                *[connected(p[i], p[j]) for i, j in platform.connectivity_graph],
+                *[next_depth(d[i], d[i + 1]) for i in range(maximum_depth - 1)],
+                *[clock(pi, d[0]) for pi in p],
+            ],
+            goal_state=[
+                *[done(gi) for gi in g],
+                *[not_(is_swapping(pi)) for pi in p],
+            ],
+        )
 
     def parse_solution(self, solution: str) -> QuantumCircuit:
         # FIXME
--- src/synthesizers/synthesizer.py
+++ src/synthesizers/synthesizer.py
@@ -46,9 +46,28 @@ class Synthesizer(ABC):
         return physical_circuit, time_taken
 
 
-def gate_input_mapping(
+def gate_line_dependency_mapping(
     circuit: QuantumCircuit,
 ) -> dict[int, tuple[str, list[int]]]:
+    """
+    Returns a mapping of gate index to the name of the gate and the qubits it acts on.
+
+    Example
+    -------
+    Given circuit:
+         ┌───┐
+    q_0: ┤ X ├──■──
+         ├───┤┌─┴─┐
+    q_1: ┤ X ├┤ X ├
+         └───┘├───┤
+    q_2: ──■──┤ X ├
+         ┌─┴─┐└───┘
+    q_3: ┤ X ├─────
+         └───┘
+
+    The mapping would be:
+    `{0: ('x', [0]), 1: ('x', [1]), 2: ('cx', [2, 3]), 3: ('cx', [0, 1]), 4: ('x', [2])}`
+    """
     circuit_data = list(circuit.data)
 
     mapping = {}
@@ -67,4 +86,53 @@ def gate_input_mapping(
             raise ValueError(f"Gate at index {i} has an input with no index.")
 
         mapping[i] = (name, input_idxs)
+
+    return mapping
+
+
+def gate_direct_dependency_mapping(circuit: QuantumCircuit) -> dict[int, list[int]]:
+    """
+    Returns a mapping of gate index to the indices of the gates that it directly depends on.
+
+    The algorithm is O(n^2) and it works like this:
+    - It calculates the line dependency mapping.
+    - Do a reverse traversal of the line dependency mapping (starting with the largest gate index)
+        - Take a given physical qubit line in the line dependency mapping for the current gate
+            - Find the largest gate index that depends on the current physical qubit line and note the gate index
+
+    Example
+    -------
+    Given circuit:
+         ┌───┐
+    q_0: ┤ X ├──■──
+         ├───┤┌─┴─┐
+    q_1: ┤ X ├┤ X ├
+         └───┘├───┤
+    q_2: ──■──┤ X ├
+         ┌─┴─┐└───┘
+    q_3: ┤ X ├─────
+         └───┘
+
+    The mapping would be:
+    `{4: [2], 3: [1, 0], 2: [], 1: [], 0: []}`
+    """
+    line_dependency_mapping = gate_line_dependency_mapping(circuit)
+
+    mapping = {}
+    for i in range(len(line_dependency_mapping) - 1, -1, -1):
+        gate_lines = line_dependency_mapping[i][1]
+        mapping[i] = []
+        for j in range(i - 1, -1, -1):
+            other_gate_lines = line_dependency_mapping[j][1]
+            for qubit in gate_lines:
+                if qubit in other_gate_lines:
+                    mapping[i].append(j)
+
+            for other_qubit in other_gate_lines:
+                if other_qubit in gate_lines:
+                    gate_lines.remove(other_qubit)
+
+            if len(gate_lines) == 0:
+                break
+
     return mapping
