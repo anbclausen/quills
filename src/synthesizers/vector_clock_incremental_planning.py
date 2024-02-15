3c00a95dc3c7ad6656cbaa0881f78aeed0dcdab7
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-02-15 15:07:54 +0100

    fix: called max_depth now

--- src/synthesizers/vector_clock_incremental_planning.py
+++ src/synthesizers/vector_clock_incremental_planning.py
@@ -26,7 +26,6 @@ class VectorClockIncrementalPlanningSynthesizer(Synthesizer):
         max_depth: int | None = None,
     ) -> PDDLInstance:
 
-
         if max_depth == None:
             raise ValueError(
                 "'max_depth' should always be given for incremental encodings"
@@ -283,9 +282,7 @@ class VectorClockIncrementalPlanningSynthesizer(Synthesizer):
         total_time = 0
         for depth in range(circuit_depth, 4 * circuit_depth + 1, 1):
             print(f"Depth: {depth}")
-            instance = self.create_instance(
-                logical_circuit, platform, maximum_depth=depth
-            )
+            instance = self.create_instance(logical_circuit, platform, max_depth=depth)
             domain, problem = instance.compile()
 
             print("Solving")
