f3ba81bebc616e35ac87ce79a34db76275b15093
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-02-19 23:17:28 +0100

    feat: added optimal planning encoding with costs

--- src/synthesizers/optimal_planning.py
+++ src/synthesizers/optimal_planning.py
@@ -10,7 +10,7 @@ from synthesizers.synthesizer import (
 )
 from platforms import Platform
 from qiskit import QuantumCircuit
-from pddl import PDDLInstance, PDDLAction, PDDLPredicate, object_, not_
+from pddl import PDDLInstance, PDDLAction, PDDLPredicate, object_, not_, increase_cost
 from solvers import (
     Solver,
     SolverSolution,
@@ -34,16 +34,12 @@ class OptimalPlanningSynthesizer(Synthesizer):
         class gate(object_):
             pass
 
-        class depth(object_):
-            pass
-
-        class lqubit(gate):
+        class lqubit(object_):
             pass
 
         p = [pqubit(f"p{i}") for i in range(num_pqubits)]
         l = [lqubit(f"l{i}") for i in range(num_lqubits)]
         g = [gate(f"g{i}") for i in range(num_gates)]
-        d = [depth(f"d{i}") for i in range(maximum_depth)]
 
         @PDDLPredicate()
         def occupied(p: pqubit):
@@ -62,74 +58,70 @@ class OptimalPlanningSynthesizer(Synthesizer):
             pass
 
         @PDDLPredicate()
-        def clock(p: pqubit, d: depth):
+        def busy(l: lqubit):
             pass
 
         @PDDLPredicate()
-        def next_depth(d1: depth, d2: depth):
+        def is_swapping1(l1: lqubit, l2: lqubit):
             pass
 
         @PDDLPredicate()
-        def next_swap_depth(d1: depth, d2: depth):
+        def is_swapping2(l1: lqubit, p2: lqubit):
             pass
 
         @PDDLPredicate()
-        def is_swapping1(p1: pqubit, p2: pqubit):
-            pass
-
-        @PDDLPredicate()
-        def is_swapping2(p1: pqubit, p2: pqubit):
-            pass
-
-        @PDDLPredicate()
-        def is_swapping(p: pqubit):
+        def is_swapping(l: lqubit):
             pass
 
         @PDDLAction()
-        def swap(l1: lqubit, l2: lqubit, p1: pqubit, p2: pqubit, d1: depth, d2: depth):
+        def swap(l1: lqubit, l2: lqubit, p1: pqubit, p2: pqubit):
             preconditions = [
                 mapped(l1, p1),
                 mapped(l2, p2),
                 connected(p1, p2),
-                next_swap_depth(d1, d2),
-                clock(p1, d1),
-                clock(p2, d1),
+                not_(busy(l1)),
+                not_(busy(l2)),
             ]
             effects = [
                 not_(mapped(l1, p1)),
                 not_(mapped(l2, p2)),
                 mapped(l1, p2),
                 mapped(l2, p1),
-                not_(clock(p1, d1)),
-                not_(clock(p2, d1)),
-                clock(p1, d2),
-                clock(p2, d2),
-                is_swapping1(p1, p2),
-                is_swapping(p1),
-                is_swapping(p2),
+                busy(l1),
+                busy(l2),
+                is_swapping1(l1, l2),
+                is_swapping(l1),
+                is_swapping(l2),
             ]
             return preconditions, effects
 
         @PDDLAction()
-        def swap_dummy1(p1: pqubit, p2: pqubit):
-            preconditions = [is_swapping1(p1, p2)]
-            effects = [not_(is_swapping1(p1, p2)), is_swapping2(p1, p2)]
-            return preconditions, effects
-
-        @PDDLAction()
-        def swap_dummy2(p1: pqubit, p2: pqubit):
-            preconditions = [is_swapping2(p1, p2)]
+        def swap_dummy1(l1: lqubit, l2: lqubit):
+            preconditions = [is_swapping1(l1, l2), not_(busy(l1)), not_(busy(l2))]
             effects = [
-                not_(is_swapping2(p1, p2)),
-                not_(is_swapping(p1)),
-                not_(is_swapping(p2)),
+                not_(is_swapping1(l1, l2)),
+                is_swapping2(l1, l2),
+                busy(l1),
+                busy(l2),
             ]
             return preconditions, effects
 
         @PDDLAction()
-        def nop(p: pqubit, d1: depth, d2: depth):
-            preconditions = [next_depth(d1, d2), clock(p, d1), not_(is_swapping(p))]
-            effects = [clock(p, d2), not_(clock(p, d1))]
+        def swap_dummy2(l1: lqubit, l2: lqubit):
+            preconditions = [is_swapping2(l1, l2), not_(busy(l1)), not_(busy(l2))]
+            effects = [
+                not_(is_swapping2(l1, l2)),
+                not_(is_swapping(l1)),
+                not_(is_swapping(l2)),
+                busy(l1),
+                busy(l2),
+            ]
+            return preconditions, effects
+
+        @PDDLAction()
+        def advance():
+            preconditions = []
+            effects = [not_(busy(l)) for l in l] + [increase_cost(1)]
             return preconditions, effects
 
         gate_line_mapping = gate_line_dependency_mapping(circuit)
@@ -144,15 +136,10 @@ class OptimalPlanningSynthesizer(Synthesizer):
                 case "cx":
 
                     @PDDLAction(name=f"apply_cx_g{gate_id}")
-                    def apply_gate(p1: pqubit, p2: pqubit, d1: depth, d2: depth):
+                    def apply_gate(p1: pqubit, p2: pqubit):
                         preconditions = [
                             not_(done(g[gate_id])),
-                            next_depth(d1, d2),
                             connected(p1, p2),
-                            clock(p1, d1),
-                            clock(p2, d1),
-                            not_(is_swapping(p1)),
-                            not_(is_swapping(p2)),
                         ]
 
                         one_gate_dependency = len(gate_direct_mapping[gate_id]) == 1
@@ -196,6 +183,12 @@ class OptimalPlanningSynthesizer(Synthesizer):
                                     occupied_physical_qubit,
                                 )
                             )
+                            preconditions.append(
+                                not_(is_swapping(l[gate_occupied_logical_qubit]))
+                            )
+                            preconditions.append(
+                                not_(busy(l[gate_occupied_logical_qubit]))
+                            )
 
                             # preconds for the unoccupied line
                             preconditions.append(
@@ -210,13 +203,15 @@ class OptimalPlanningSynthesizer(Synthesizer):
 
                             preconditions.append(mapped(control_qubit, p1))
                             preconditions.append(mapped(target_qubit, p2))
+                            preconditions.append(not_(is_swapping(control_qubit)))
+                            preconditions.append(not_(is_swapping(target_qubit)))
+                            preconditions.append(not_(busy(control_qubit)))
+                            preconditions.append(not_(busy(target_qubit)))
 
                         effects = [
                             done(g[gate_id]),
-                            not_(clock(p1, d1)),
-                            not_(clock(p2, d1)),
-                            clock(p1, d2),
-                            clock(p2, d2),
+                            busy(l[gate_logical_qubits[0]]),
+                            busy(l[gate_logical_qubits[1]]),
                         ]
 
                         if no_gate_dependency:
@@ -224,6 +219,7 @@ class OptimalPlanningSynthesizer(Synthesizer):
                             effects.append(occupied(p2))
                             effects.append(mapped(l[gate_logical_qubits[0]], p1))
                             effects.append(mapped(l[gate_logical_qubits[1]], p2))
+
                         elif one_gate_dependency:
                             earlier_gate = direct_predecessor_gates[0]
                             _, earlier_gate_logical_qubits = gate_line_mapping[
@@ -262,14 +258,11 @@ class OptimalPlanningSynthesizer(Synthesizer):
                 case _:
 
                     @PDDLAction(name=f"apply_gate_g{gate_id}")
-                    def apply_gate(p: pqubit, d1: depth, d2: depth):
+                    def apply_gate(p: pqubit):
                         logical_qubit = l[gate_logical_qubits[0]]
 
                         preconditions = [
                             not_(done(g[gate_id])),
-                            next_depth(d1, d2),
-                            clock(p, d1),
-                            not_(is_swapping(p)),
                         ]
 
                         if no_gate_dependency:
@@ -278,12 +271,10 @@ class OptimalPlanningSynthesizer(Synthesizer):
                             direct_predecessor_gate = g[direct_predecessor_gates[0]]
                             preconditions.append(done(direct_predecessor_gate))
                             preconditions.append(mapped(logical_qubit, p))
+                            preconditions.append(not_(is_swapping(logical_qubit)))
+                            preconditions.append(not_(busy(logical_qubit)))
 
-                        effects = [
-                            done(g[gate_id]),
-                            clock(p, d2),
-                            not_(clock(p, d1)),
-                        ]
+                        effects = [done(g[gate_id]), busy(logical_qubit)]
 
                         if no_gate_dependency:
                             effects.append(occupied(p))
@@ -294,17 +285,15 @@ class OptimalPlanningSynthesizer(Synthesizer):
             gate_actions.append(apply_gate)
 
         return PDDLInstance(
-            types=[pqubit, lqubit, gate, depth],
-            constants=[*l, *g, *d],
+            types=[pqubit, lqubit, gate],
+            constants=[*l, *g],
             objects=[*p],
             predicates=[
                 occupied,
                 mapped,
                 connected,
                 done,
-                clock,
-                next_depth,
-                next_swap_depth,
+                busy,
                 is_swapping1,
                 is_swapping2,
                 is_swapping,
@@ -313,19 +302,17 @@ class OptimalPlanningSynthesizer(Synthesizer):
                 swap,
                 swap_dummy1,
                 swap_dummy2,
-                nop,
+                advance,
                 *gate_actions,
             ],
             initial_state=[
                 *[connected(p[i], p[j]) for i, j in platform.connectivity_graph],
-                *[next_depth(d[i], d[i + 1]) for i in range(maximum_depth - 1)],
-                *[next_swap_depth(d[i], d[i + 3]) for i in range(1, maximum_depth - 3)],
-                *[clock(pi, d[0]) for pi in p],
             ],
             goal_state=[
                 *[done(gi) for gi in g],
-                *[not_(is_swapping(pi)) for pi in p],
+                *[not_(is_swapping(li)) for li in l],
             ],
+            cost_function=True,
         )
 
     def synthesize(
