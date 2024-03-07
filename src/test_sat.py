998d944c8d8ada8d4515bafca14d45f202cb4441
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-03-07 15:17:35 +0100

    feat: added goal state

--- src/test_sat.py
+++ src/test_sat.py
@@ -80,131 +80,149 @@ swap = {
     for t in range(max_depth + 1)
 }
 
-for t in range(0, max_depth + 1):
-    # mappings and occupancy
-    for l in lq:
-        f = exactly_one([mapped[0][l][p] for p in pq])
-        solver.append_formula(f)
-    for p in pq:
-        f = at_most_one([mapped[0][l][p] for l in lq])
-        solver.append_formula(f)
-    for p in pq:
-        f = Iff(Or(*[mapped[t][l][p] for l in lq]), occupied[t][p]).clausify()
-        solver.append_formula(f)
-
-    # cnot connections
-    inner = []
-    for l, l_prime in lq_pairs:
-        conj1 = And(
-            *[
-                (mapped[t][l][p] & mapped[t][l_prime][p_prime])
-                >> enabled[t][l][l_prime]
-                for p, p_prime in connectivity_graph
-            ]
-        )
-        conj2 = And(
-            *[
-                (mapped[t][l][p] & mapped[t][l_prime][p_prime])
-                >> ~enabled[t][l][l_prime]
-                for p, p_prime in inv_connectivity_graph
-            ]
-        )
-        inner.append(conj1 & conj2)
-    f = And(*inner).clausify()
-    solver.append_formula(f)
-
-    # if gate 1 (cnot) is scheduled, then l2 and l3 are connected
-    # similar for gate 2
-    # FIXME: Hardcoded, should be generated from the circuit
-    f = (
-        current[t][1] >> enabled[t][2][3] & current[t][2] >> enabled[t][0][1]
-    ).clausify()
-    solver.append_formula(f)
-
-    # gate stuff
-    # FIXME: require swap-free!
-    for g in gates:
-        f = exactly_one([current[t][g], advanced[t][g], delayed[t][g]])
-        solver.append_formula(f)
-
-        f = And(*[current[t][g] >> advanced[t][pred] for pred in pre[g]]).clausify()
-        solver.append_formula(f)
-
-        f = And(*[current[t][g] >> advanced[t][succ] for succ in succ[g]]).clausify()
-        solver.append_formula(f)
-
-        if t > 0:
-            f = (advanced[t][g] >> (current[t - 1][g] | advanced[t - 1][g])).clausify()
+for tmax in range(circuit_depth, max_depth + 1):
+    for t in range(0, tmax):
+        # mappings and occupancy
+        for l in lq:
+            f = exactly_one([mapped[0][l][p] for p in pq])
+            solver.append_formula(f)
+        for p in pq:
+            f = at_most_one([mapped[0][l][p] for l in lq])
+            solver.append_formula(f)
+        for p in pq:
+            f = Iff(Or(*[mapped[t][l][p] for l in lq]), occupied[t][p]).clausify()
             solver.append_formula(f)
 
-            f = (delayed[t - 1][g] >> (current[t][g] | delayed[t][g])).clausify()
+        # cnot connections
+        inner = []
+        for l, l_prime in lq_pairs:
+            conj1 = And(
+                *[
+                    (mapped[t][l][p] & mapped[t][l_prime][p_prime])
+                    >> enabled[t][l][l_prime]
+                    for p, p_prime in connectivity_graph
+                ]
+            )
+            conj2 = And(
+                *[
+                    (mapped[t][l][p] & mapped[t][l_prime][p_prime])
+                    >> ~enabled[t][l][l_prime]
+                    for p, p_prime in inv_connectivity_graph
+                ]
+            )
+            inner.append(conj1 & conj2)
+        f = And(*inner).clausify()
+        solver.append_formula(f)
+
+        # if gate 1 (cnot) is scheduled, then l2 and l3 are connected
+        # similar for gate 2
+        # FIXME: Hardcoded, should be generated from the circuit
+        f = (
+            current[t][1] >> enabled[t][2][3] & current[t][2] >> enabled[t][0][1]
+        ).clausify()
+        solver.append_formula(f)
+
+        # gate stuff
+        # FIXME: require swap-free!
+        for g in gates:
+            f = exactly_one([current[t][g], advanced[t][g], delayed[t][g]])
             solver.append_formula(f)
 
-        f = And(*[advanced[t][g] >> advanced[t][pred] for pred in pre[g]]).clausify()
-        solver.append_formula(f)
+            f = And(*[current[t][g] >> advanced[t][pred] for pred in pre[g]]).clausify()
+            solver.append_formula(f)
 
-        f = And(*[delayed[t][g] >> delayed[t][succ] for succ in succ[g]]).clausify()
-        solver.append_formula(f)
-
-    # swap stuff
-    for l in lq:
-        f = exactly_one([free[t][l], swap1[t][l], swap2[t][l], swap3[t][l]])
-        solver.append_formula(f)
-
-        f = at_most_one(
-            [swap[t][l][l_prime] for l_prime in lq]
-            + [swap[t][l_prime][l] for l_prime in lq]
-        )
-        solver.append_formula(f)
-
-        if t > 0:
             f = And(
-                *[free[t][l] >> Iff(mapped[t - 1][l][p], mapped[t][l][p]) for p in pq]
+                *[current[t][g] >> advanced[t][succ] for succ in succ[g]]
             ).clausify()
             solver.append_formula(f)
 
-            f = Iff(swap1[t - 1][l], swap2[t][l]).clausify()
+            if t > 0:
+                f = (
+                    advanced[t][g] >> (current[t - 1][g] | advanced[t - 1][g])
+                ).clausify()
+                solver.append_formula(f)
+
+                f = (delayed[t - 1][g] >> (current[t][g] | delayed[t][g])).clausify()
+                solver.append_formula(f)
+
+            f = And(
+                *[advanced[t][g] >> advanced[t][pred] for pred in pre[g]]
+            ).clausify()
             solver.append_formula(f)
 
-            f = Iff(swap2[t - 1][l], swap3[t][l]).clausify()
+            f = And(*[delayed[t][g] >> delayed[t][succ] for succ in succ[g]]).clausify()
             solver.append_formula(f)
 
-            for l_prime in lq:
+        # swap stuff
+        for l in lq:
+            f = exactly_one([free[t][l], swap1[t][l], swap2[t][l], swap3[t][l]])
+            solver.append_formula(f)
 
+            f = at_most_one(
+                [swap[t][l][l_prime] for l_prime in lq]
+                + [swap[t][l_prime][l] for l_prime in lq]
+            )
+            solver.append_formula(f)
+
+            if t > 0:
                 f = And(
                     *[
-                        swap[t][l][l_prime]
-                        >> (
-                            Iff(mapped[t - 1][l][p], mapped[t][l][p_prime])
-                            & Iff(
-                                mapped[t - 1][l_prime][p_prime], mapped[t][l_prime][p]
-                            )
-                        )
-                        for p, p_prime in connectivity_graph
+                        free[t][l] >> Iff(mapped[t - 1][l][p], mapped[t][l][p])
+                        for p in pq
                     ]
-                )
+                ).clausify()
                 solver.append_formula(f)
-        for l_prime in lq:
-            f = swap[t][l][l_prime] >> enabled[t][l][l_prime]
-            solver.append_formula(f)
 
-            f = swap[t][l][l_prime] >> (
-                (swap1[t][l] & swap1[t][l_prime])
-                | (swap2[t][l] & swap2[t][l_prime])
-                | (swap3[t][l] & swap3[t][l_prime])
-            )
+                f = Iff(swap1[t - 1][l], swap2[t][l]).clausify()
+                solver.append_formula(f)
+
+                f = Iff(swap2[t - 1][l], swap3[t][l]).clausify()
+                solver.append_formula(f)
+
+                for l_prime in lq:
+
+                    f = And(
+                        *[
+                            swap[t][l][l_prime]
+                            >> (
+                                Iff(mapped[t - 1][l][p], mapped[t][l][p_prime])
+                                & Iff(
+                                    mapped[t - 1][l_prime][p_prime],
+                                    mapped[t][l_prime][p],
+                                )
+                            )
+                            for p, p_prime in connectivity_graph
+                        ]
+                    )
+                    solver.append_formula(f)
+            for l_prime in lq:
+                f = swap[t][l][l_prime] >> enabled[t][l][l_prime]
+                solver.append_formula(f)
+
+                f = swap[t][l][l_prime] >> (
+                    (swap1[t][l] & swap1[t][l_prime])
+                    | (swap2[t][l] & swap2[t][l_prime])
+                    | (swap3[t][l] & swap3[t][l_prime])
+                )
 
     # goal
-    # nothing delayed at the end
-    # no swap1 and swap2
+    f = And(*[~delayed[tmax][g] for g in gates]).clausify()
+    solver.append_formula(f)
+
+    f = And(*[~swap1[tmax][l] for l in lq]).clausify()
+    solver.append_formula(f)
+
+    f = And(*[~swap2[tmax][l] for l in lq]).clausify()
+    solver.append_formula(f)
 
     solver.solve()
     solution = parse_solution(solver.get_model())
     if solution:
-        print(f"{t}: Solution found")
+        print(f"{tmax}: Solution found")
         for atom in solution:
             print(atom)
 
         break
     else:
-        print(f"{t}: No solution")
+        print(f"{tmax}: No solution")
+        solver = Glucose42()
