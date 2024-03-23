2d62802c1430ad88ecb905a04c4a34cbb8804b79
Anders B. Clausen <anbclausen@users.noreply.github.com>
2024-03-23 13:46:35 +0100

    refactor: only resetting solver when dealing with sat-based synthesizers

--- src/experiments.py
+++ src/experiments.py
@@ -1,6 +1,5 @@
 import os
 import json
-import pysat.solvers
 
 from qiskit import QuantumCircuit
 from typing import Literal
@@ -243,7 +242,8 @@ for synthesizer_name, synthesizer_instance in synthesizers.items():
 for input_file, platform_name in EXPERIMENTS:
     results: dict[tuple[str, str], tuple[int, int, float] | Literal["NS", "TO"]] = {}
     for synthesizer_name, solver_name in configurations:
-        solvers[solver_name] = solvers[solver_name].__class__()
+        if not isinstance(solvers[solver_name], planning.Solver):
+            solvers[solver_name] = solvers[solver_name].__class__()
 
         print(
             f"Running '{synthesizer_name}' on '{solver_name}' for 'benchmarks/{input_file}' on '{platform_name}'..."
